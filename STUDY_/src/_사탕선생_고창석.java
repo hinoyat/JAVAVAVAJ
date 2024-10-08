05cb9dcdea5600943ffddbaa804d74b11717aef3
Hinoyat <hhwj2270@gmail.com>
2024-10-08 17:46:34 +0900

    241008 1746

---
+++ STUDY_/src/_사탕선생_고창석.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class _사탕선생_고창석 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 0; tc < T; tc++) {
+			br.readLine();
+			BigInteger N = new BigInteger(br.readLine());
+			BigInteger sum_candy = BigInteger.ZERO;
+			for (int n = 0; n < N.intValue() ; n++) {
+				BigInteger candy = new BigInteger(br.readLine());
+				sum_candy = sum_candy.add(candy);
+			}
+//			System.out.println("사탕 합" + sum_candy);
+			int pos = sum_candy.remainder(N).intValue();
+//			System.out.println("나머지" + pos);
+			if (pos == 0) {
+				System.out.println("YES");
+			}else {
+				System.out.println("NO");
+			}
+		}
+		
+	}
+}
